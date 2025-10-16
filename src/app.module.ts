1fbd26c28c02ad34b501b5df4fc8ec779951a3a9
Ivan Cardozo <ivancardozo11@hotmail.com>
2025-10-16 21:05:54 +0200

    setup and initial packages added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.register({ isGlobal: true, ttl: 10000 }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
